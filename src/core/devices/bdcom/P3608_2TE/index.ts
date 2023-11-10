65e06bed9da054a7e61067454a350e9111fad718
Andrii Zhovtiak <andy@urlog.net>
2023-11-10 13:02:33 +0200

    Added BDCOM P3608-2TE model configuration

---
+++ src/core/devices/bdcom/P3608_2TE/index.ts
@@ -0,0 +1,20 @@
+import {generic} from "../generic";
+import {IDeviceConfiguration} from "../../../network/interfaces/IDeviceConfiguration";
+
+const messageAuthFailed = "Authentication failed";
+export const P3608_2TE: { configuration: IDeviceConfiguration, commands: any } = {
+    configuration: {
+        ...generic.configuration,
+        messageAuthFailed,
+        portsCount: {
+            ...generic.configuration.portsCount,
+            PON: 8,
+            GE: 8,
+            ETH: 1,
+            XGE: 2
+        }
+    },
+    commands: {
+        ...generic.commands
+    }
+}
--- src/core/devices/bdcom/index.ts
+++ src/core/devices/bdcom/index.ts
@@ -1,6 +1,9 @@
 import {generic} from "./generic";
 import {P3310D} from "./P3310D";
+import {P3608_2TE} from "./P3608_2TE";
+
 export const BDCOM = {
     generic,
-    P3310D
+    P3310D,
+    P3608_2TE
 }
