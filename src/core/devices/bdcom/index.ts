886a57ef3de5954c228dac0feaac137bfdc35c3b
Andrii Zhovtiak <andy@urlog.net>
2023-11-21 11:23:34 +0200

    Added P3608B device configuration

--- src/core/devices/bdcom/index.ts
+++ src/core/devices/bdcom/index.ts
@@ -3,11 +3,13 @@ import {P3310D} from "./P3310D";
 import {P3608_2TE} from "./P3608_2TE";
 import {P3600_08E} from "./P3600_08E";
 import {GP3600_08B} from "./GP3600-08B";
+import {P3608B} from "./P3608B_08E";
 
 export const BDCOM = {
     generic,
     P3310D,
     P3608_2TE,
     P3600_08E,
-    GP3600_08B
+    GP3600_08B,
+    P3608B
 }
