4516975d51280a10940623758dd7ccd6ca48f59d
Andrii Zhovtiak <andy@urlog.net>
2024-01-19 11:21:11 +0200

    Added indexes

---
+++ prisma/migrations/20240119090315_stat_onu_device_add_indexes/down.sql
@@ -0,0 +1,12 @@
+-- DropIndex
+DROP INDEX "StatOnuDevice_xponBoard_idx";
+
+-- DropIndex
+DROP INDEX "StatOnuDevice_xponPort_idx";
+
+-- DropIndex
+DROP INDEX "StatOnuDevice_xponInterface_idx";
+
+-- DropIndex
+DROP INDEX "StatOnuDevice_networkDeviceId_idx";
+
---
+++ prisma/migrations/20240119090315_stat_onu_device_add_indexes/migration.sql
@@ -0,0 +1,11 @@
+-- CreateIndex
+CREATE INDEX "StatOnuDevice_xponBoard_idx" ON "StatOnuDevice"("xponBoard");
+
+-- CreateIndex
+CREATE INDEX "StatOnuDevice_xponPort_idx" ON "StatOnuDevice"("xponPort");
+
+-- CreateIndex
+CREATE INDEX "StatOnuDevice_xponInterface_idx" ON "StatOnuDevice"("xponInterface");
+
+-- CreateIndex
+CREATE INDEX "StatOnuDevice_networkDeviceId_idx" ON "StatOnuDevice"("networkDeviceId");
